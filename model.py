0c54a33bb5c27091c38b9c27b7f5dfe8016e3166
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-08-01 00:03:15 +0800

    archiving repo

--- model.py
+++ model.py
@@ -1,47 +1,47 @@
 import numpy as np
 import seaborn as sns
-from kunikchon import get_df
 import matplotlib.pyplot as plt
+from imblearn.pipeline import Pipeline
 from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import cross_validate, cross_val_predict
+
+from connections import get_dataframe
 
 
-df = get_df()
+df = get_dataframe() # database(mysql): datasets, table: `ph-student_employability-dataset`
 
+df['CLASS'] = df['CLASS'].map({'Employable': 1, 'LessEmployable': 2})
 
-df['CLASS'] = df['CLASS'].map({'Employable':1, 'LessEmployable':2})
-
-
-X = df.drop({'Name of Student', 'CLASS'}, axis=1)
+X = df.drop(['Name of Student', 'CLASS'], axis=1)
 y = df['CLASS']
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+pipeline = Pipeline([
+    ('scaler', StandardScaler()),
+    ('ismot', SMOTE(random_state=42)),
+    ('classifier', RandomForestClassifier(random_state=42))
+])
+
+cv_results = cross_validate(pipeline, X, y, cv=5, scoring=['accuracy', 'precision_macro', 'recall_macro', 'f1_macro'])
+
+print("Cross-validation results:", cv_results)
+print("Mean accuracy:", cv_results['test_accuracy'].mean())
+print("Mean precision:", cv_results['test_precision_macro'].mean())
+print("Mean recall:", cv_results['test_recall_macro'].mean())
+print("Mean F1-score:", cv_results['test_f1_macro'].mean())
 
 
-smote = SMOTE(random_state=42)
-X_train_b, y_train_b = smote.fit_resample(X_train, y_train)
+y_pred = cross_val_predict(pipeline, X, y, cv=5)
+cm = confusion_matrix(y, y_pred)
 
+labels = np.unique(y) 
+print('classification_report:\n', classification_report(y, y_pred))
+sns.heatmap(cm, annot=True, fmt='d', xticklabels=labels, yticklabels=labels, cbar=0)
 
-rf_class_model = RandomForestClassifier()
-rf_class_model.fit(X_train_b, y_train_b)
-y_pred = rf_class_model.predict(X_test)
-
-
-cr = classification_report(y_test, y_pred); print(cr)
-conf = confusion_matrix(y_test, y_pred)
-
-
-index =  np.unique(y)
-
-sns.heatmap(conf, annot=True, xticklabels=index, yticklabels=index, fmt='d')
-plt.xlabel('predicted')
+comment = '1: Employable\n2: Less Employable'
+plt.text(0.05, 0.2, comment, wrap=True, horizontalalignment='left', fontsize=10)
 plt.ylabel('actual')
-plt.title('1-employable, 2-lessEmployable ph students')
+plt.xlabel('predicted')
 plt.show()
-
-
